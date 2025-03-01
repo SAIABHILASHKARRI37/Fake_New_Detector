4855c7bad9306c4c79d9a4b2ba2e7bd12e1d3ca2
Abhilash_37 <140600387+SAIABHILASHKARRI37@users.noreply.github.com>
2025-03-01 14:19:52 +0530

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -1,28 +1,15 @@
 import streamlit as st
-import joblib
+import requests
 
-# Load the trained model & vectorizer
-model = joblib.load("fake_news_model.pkl")
-vectorizer = joblib.load("vectorizer.pkl")
+st.title("📰 Fake News Detection")
 
-# Streamlit UI
-st.title("📰 Fake News Detection App")
-
-# User input text box
-user_input = st.text_area("Enter news text:", "")
+user_input = st.text_area("Enter News Article Text")
 
 if st.button("Check Authenticity"):
-    if user_input.strip() == "":
-        st.warning("Please enter some text!")
+    response = requests.post("http://127.0.0.1:5000/predict", json={"text": user_input})
+    
+    if response.status_code == 200:
+        result = response.json()['prediction']
+        st.success(f"The news article is classified as: *{result}*")
     else:
-        # Convert input text to feature vector
-        transformed_text = vectorizer.transform([user_input])
-        
-        # Predict using the loaded model
-        prediction = model.predict(transformed_text)[0]
-
-        # Display result
-        if prediction == 1:
-            st.error("🚨 This is Real News!")
-        else:
-            st.success("✅ This is Fake News!")
+        st.error("Error in prediction")
